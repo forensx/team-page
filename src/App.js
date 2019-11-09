a70c4d9b629287b59180bc5481dfa94248d1c41c
Aniket Pant <pant.aniket@gmail.com>
2019-11-08 18:42:26 -0600

    Added deck.GL demo and styling for draft

--- src/App.js
+++ src/App.js
@@ -4,17 +4,20 @@ import NavbarOverlay from "./Components/NavbarOverlay";
 import DeckGL from '@deck.gl/react';
 import { ArcLayer } from '@deck.gl/layers';
 import { StaticMap } from 'react-map-gl';
+import TeamPageExceptImage from "./team_page_excerpt_image.svg";
 
 // Set your mapbox access token here
 const MAPBOX_ACCESS_TOKEN = 'pk.eyJ1IjoicGFudDIwMDIiLCJhIjoiY2prenlwb2ZtMHlnMjNxbW1ld3VxYWZ4cCJ9.rOb8DhCzsysBIw69MxyWKg';
 
 // Initial viewport settings
 const initialViewState = {
-  longitude: -122.41669,
-  latitude: 37.7853,
+  longitude: -122.4230,
+  latitude: 37.8270,
   zoom: 13,
-  pitch: 15,
-  bearing: 30
+  pitch: 60,
+  bearing: 10,
+  width: 500,
+  height: 500
 };
 
 // Data to be used by the LineLayer
@@ -29,24 +32,43 @@ function App() {
 
   return (
     <div className="App">
-      <div className="siteIntro">
-        <div className="introHeader">
-          Your <div className="inlineHeaderEmphasis">personal data</div> forensics
-        </div>
-        <div className="introCaption">
-          Utilize the power of data analytics to drive your business’s profits up and operating costs down.
+      <div id className="scrollRegion1">
+        <div className="siteIntro">
+          <div className="introHeader">
+            Your <div className="inlineHeaderEmphasis">personal data</div> forensics
           </div>
-        <button className="transitionButton">
-          Learn more
-        </button>
+          <div className="introCaption">
+            Utilize the power of data analytics to drive your business’s profits up and operating costs down.
+            </div>
+          <button className="transitionButton">
+            Learn more
+          </button>
+        </div>
+        <div className = "deckGL_demo">
+          <DeckGL
+          initialViewState={initialViewState}
+          layers={layers}
+          >
+            <StaticMap mapboxApiAccessToken={MAPBOX_ACCESS_TOKEN} />
+          </DeckGL>
+        </div>
       </div>
-      <DeckGL
-        initialViewState={initialViewState}
-        controller={true}
-        layers={layers}
-      >
-        <StaticMap mapboxApiAccessToken={MAPBOX_ACCESS_TOKEN} />
-      </DeckGL>
+      <div className = "scrollRegion2">
+        <div className = "region2_excerpts">
+          <div className = "excerptHeader">
+            Don’t let your company drown in your data - sail through it.
+          </div>
+          <div className = "excerptDescription">
+            We help you make data-driven decisions to accelerate production and increase profits.
+          </div>
+          <div className = "excerptCall">
+            Let us assist with data implementation, analysis, and modeling for your company with a money back policy.
+          </div>
+          <div className = "excerptImage">
+            <img src = {TeamPageExceptImage} alt = ""></img>
+          </div>
+        </div>
+      </div>
     </div >
   );
 }
